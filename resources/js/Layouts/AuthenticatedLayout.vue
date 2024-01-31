063a9be7105ad625a6b5b8975257e3f5f4b36c59
motooga <hashioga2017@gmail.com>
2024-02-01 05:43:18 +0900

    かっこ抜け修正

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -44,10 +44,9 @@ const showingNavigationDropdown = ref(false);
                                             <button
                                                 type="button"
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-
-                                                {{ $page.props.auth.user.family_name }}
-
-                                                <svg>
+                                            >
+                                            {{ $page.props.auth.user.family_name }}
+                                                <svg
                                                     class="ms-2 -me-0.5 h-4 w-4"
                                                     xmlns="http://www.w3.org/2000/svg"
                                                     viewBox="0 0 20 20"
